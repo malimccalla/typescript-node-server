609a7d21c581b3d873841543873eeb0b3617f9de
Mali Michael <malimccalla@gmail.com>
2018-03-04 12:09:35 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { Context } from 'vm';
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `Hello ${name || 'Worl'}`,
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on http://localhost:4000'));
